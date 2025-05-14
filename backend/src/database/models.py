54d56ebced4c9b6c75d41f1b29e6480fcfd455d2
Raj Shah <rajshah9305@gmail.com>
2025-05-14 14:51:27 -0600

    Update models.py

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, Enum
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, Enum, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 import enum
